4f6fa3088b92bd11627eec73e663c0b3e467088e
Mateusz Kuźnik <mateusz.kuznik@azimo.com>
2022-05-17 20:32:44 +0200

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Mateusz Kuznik on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
