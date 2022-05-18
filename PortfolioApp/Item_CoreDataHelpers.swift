2923b1e54c5c624867789b2a446ac9ccdc09fd82
Mateusz Kuźnik <mateusz.kuznik@azimo.com>
2022-05-18 21:46:47 +0200

    Cleaning up Core Data

---
+++ PortfolioApp/Item_CoreDataHelpers.swift
@@ -0,0 +1,37 @@
+//
+//  Item_CoreDataHelpers.swift
+//  PortfolioApp
+//
+//  Created by Mateusz Kuznik on 18/05/2022.
+//
+
+import Foundation
+
+
+extension Item {
+
+    var itemTitle: String {
+        title ?? ""
+    }
+
+    var itemDetail: String {
+        detail ?? ""
+    }
+
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+
+        return item
+    }
+}
