31c0755da0dc500da6bb4beecc6957726a0d2df4
Mateusz Kuźnik <mateusz.kuznik@azimo.com>
2022-05-24 21:28:00 +0200

    Reading awards JSON

--- PortfolioApp/ContentView.swift
+++ PortfolioApp/ContentView.swift
@@ -30,6 +30,13 @@ struct ContentView: View {
                     Image(systemName: "checkmark")
                     Text("Closed")
                 }
+
+            AwardsView()
+                .tag(AwardsView.tag)
+                .tabItem {
+                    Image(systemName: "rosette")
+                    Text("Awards")
+                }
         }
     }
 }
